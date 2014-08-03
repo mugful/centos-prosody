a4cfdc8e0958c1f50d92c68aa742db8c461489a6
Jiří Stránský <jistr@jistr.com>
2014-08-03 19:41:44 +0200

    Use conf for configuration files

--- prosody-install.sh
+++ prosody-install.sh
@@ -10,7 +10,7 @@ rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.no
 yum -y install prosody sudo supervisor
 
 mv /etc/prosody/prosody.cfg.lua /etc/prosody/prosody.cfg.lua.rpm_orig
-ln -s /var/lib/prosody/etc/prosody.cfg.lua /etc/prosody/prosody.cfg.lua
+ln -s /var/lib/prosody/conf/prosody.cfg.lua /etc/prosody/prosody.cfg.lua
 
 # clean cache to keep the image small
 yum clean all
