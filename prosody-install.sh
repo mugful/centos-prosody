61f4e6cc487c945c4dc6e44d224124a8837d8841
Jiří Stránský <jistr@jistr.com>
2015-01-25 02:00:09 +0100

    Compile lua-zlib to allow using 'compression' Prosody module

--- prosody-install.sh
+++ prosody-install.sh
@@ -8,7 +8,22 @@ yum -y reinstall glibc-common
 yum -y install epel-release
 curl -o /etc/yum.repos.d/ntrrgc-prosody-epel-7.repo https://copr.fedoraproject.org/coprs/ntrrgc/prosody/repo/epel-7/ntrrgc-prosody-epel-7.repo
 
-yum -y install prosody sudo
+yum -y install gcc lua-devel make prosody tar wget zlib-devel
+
+# install lua-zlib
+LUA_ZLIB_VERSION=0.4
+pushd /root
+wget https://github.com/brimworks/lua-zlib/archive/v$LUA_ZLIB_VERSION.tar.gz
+tar -xzvf v$LUA_ZLIB_VERSION.tar.gz
+pushd lua-zlib-$LUA_ZLIB_VERSION
+export LUACPATH="/usr/lib64/lua/5.1"
+export LIBDIR="-L/usr/lib64"
+make linux
+make install
+popd
+popd
+# remove deps for lua-zlib building
+yum -y remove cpp gcc lua-devel make tar wget zlib-devel
 
 mv /etc/prosody/prosody.cfg.lua /etc/prosody/prosody.cfg.lua.rpm_orig
 ln -s /var/lib/prosody/conf/prosody.cfg.lua /etc/prosody/prosody.cfg.lua
